006398100f902191dcc471b1bde8c13b5ae41615
Predeep <predeepkumar.us2022cse@sece.ac.in>
2025-06-23 11:05:27 +0530

    Updated Footer integration

--- src/components/DFooter.jsx
+++ src/components/DFooter.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const DFooter = () => {
   return (
-   <footer className="w-full bg-white shadow py-4 ">
+    <footer className="mt-auto w-full bg-white shadow py-4">
       <div className="text-center text-md text-gray-600">
         © 2025 <span className="font-semibold text-blue-700">Infoziant</span>. All Rights Reserved.
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default DFooter
+export default DFooter;
